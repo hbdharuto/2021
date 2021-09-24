3d6bc641e174a370a012365c83641549babab5a0
hbdharuto <71276889+hbdharuto@users.noreply.github.com>
2021-09-25 03:03:25 +0800

    endingnya bikin mobile ver juga
    ngakak

--- src/hbdharuto_2021.js
+++ src/hbdharuto_2021.js
@@ -216,5 +216,3 @@ $(".catto_button").click(function() {
 
 
 
-document.body.requestFullscreen().then(res=>console.log(res).catch(err=>console.log(err);
-screen.orientation.lock('landscape').then(res=>console.log(res)).catch(err=>console.log(err));
